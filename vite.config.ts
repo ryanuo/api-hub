02eb9665adb2821df09769064bbd77e8046a6a48
ryanuo <1577791638@qq.com>
2025-05-25 00:03:52 +0800

    refactor(project): simplify project structure and update dependencies
    - Remove Cloudflare-related scripts and dependencies
    - Update `preview` script to use Vite preview
    - Move Hono dependency to dependencies section
    - Remove unused dev dependencies
    - Simplify Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,31 @@
+import build from '@hono/vite-build/node'
 import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 
-export default defineConfig(({ command, isSsrBuild }) => {
+export default defineConfig(({ command }) => {
   if (command === 'serve') {
     return {
       server: {
         port: 5148,
         host: true,
       },
-      plugins: [ssrHotReload(), devServer({
-        entry: 'src/index.tsx',
-      })],
-    }
-  }
-
-  if (!isSsrBuild) {
-    return {
-      build: {
-        rollupOptions: {
-          input: ['./src/style.css'],
-          output: {
-            assetFileNames: 'assets/[name].[ext]',
-            // 确保路由模块被单独打包
-            manualChunks(id) {
-              if (id.includes('routes')) {
-                return 'routes'
-              }
-            },
-          },
-        },
-      },
+      plugins: [
+        devServer({
+          entry: 'src/index.tsx',
+        }),
+      ],
     }
   }
 
   return {
+    plugins: [
+      build({
+        entry: 'src/index.tsx',
+      }),
+    ],
     build: {
-      ssr: true,
-      outDir: 'dist-server',
       rollupOptions: {
-        input: 'index.js', // 指定服务器入口文件
+        external: ['@hono/node-server/serve-static', '@hono/node-server'],
       },
     },
   }
